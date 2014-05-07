b1a5d14236eea73282cbdeff9e932f4cdd028193
WebDevJL <webdev.jl@gmail.com>
2014-05-07 13:18:57 +0200

    config improvements
    Made the database connection more friendly and easier to manage

--- config.php
+++ config.php
@@ -1,43 +1,55 @@
 <?php
 
-$con = mysql_connect("localhost","baiken_fwa","fwa1");
+/*
+ * To make it easier to change the DB credentials, let's make a nice array
+ * You can change the config used by modifying just the variable $db_config_used
+ * 
+ * Below, we get the value in the selected array based on the key (host, user, pwd, etc...)
+ * That way, when we change the credentials, we have to do it only in one place instead of 3.
+ */
 
+/*Configuration section*/
+$db_config_used = "local_mysql";
 
-if (!$con)
+$db_config["local_mysql"]["host"] = "localhost";
+$db_config["local_mysql"]["user"] = "baiken_fwa";
+$db_config["local_mysql"]["pwd"] = "fwa1";
+$db_config["local_mysql"]["default_db_name"] = "baiken_fwa";
 
-  {
+$db_config["remote_mysql"]["host"] = "";
+$db_config["remote_mysql"]["user"] = "baiken_fwa";
+$db_config["remote_mysql"]["pwd"] = "fwa1";
+$db_config["remote_mysql"]["default_db_name"] = "baiken_fwa";
+/*End of Configuration section*/
 
-  die('Could not connect: ' . mysql_error());
+//get the array with the database config to use
+$db = $db_config[$db_config_used];
 
-  }
+/*  Original MYSQL method   */
+//$con = mysql_connect("localhost","baiken_fwa","fwa1");
+$con = mysql_connect($db["host"], $db["user"], $db["pwd"]);
 
-// mysql_select_db('baiken_fwa',$con);
+if (!$con) {
+    die('Could not connect: ' . mysql_error());
+}
+//mysql_select_db('baiken_fwa',$con);
+mysql_select_db($db["default_db_name"], $con);
 
-mysql_select_db('baiken_fwa',$con);
-
+/*  MYSQLI method   */
 /* Open a connection */
-$mysqli = new mysqli("localhost", "baiken_fwa", "fwa1", "baiken_fwa");
+//$mysqli = new mysqli("localhost", "baiken_fwa", "fwa1", "baiken_fwa");
+$mysqli = new mysqli($db["host"], $db["user"], $db["pwd"], $db["default_db_name"]);
 
-/* check connection*/
+/* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
-} 
+}
 
-
-  # MySQL with PDO_MYSQL  
-$user=baiken_fwa;
-$pass=fwa1;
-
-try {    
-  
-
-  $DBH = new PDO("mysql:host=localhost;dbname=baiken_fwa", $user, $pass);  
-$DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/*  PDO method   */
+try {
+    $DBH = new PDO("mysql:host=" . $db["host"] . ";dbname=" . $db["default_db_name"], $db["user"], $db["pwd"]);
+    $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
 }  
-catch(PDOException $e) {  
-    echo $e->getMessage();  
-}  
-
-
-?>
